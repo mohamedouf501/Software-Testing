06e0afd78709884ad030d2a112ccc99318e51658
mohamedouf504 <mohamedouf504@gmail.com>
2022-12-07 01:04:26 +0200

    add some exampel

---
+++ db.js
@@ -0,0 +1,7 @@
+const getOrder = (orderId) => {
+  return { id: orderId, price: 200 };
+};
+
+module.exports = {
+  getOrder,
+};
---
+++ uilts.js
@@ -0,0 +1,52 @@
+const db = require("./db");
+
+const sum = (a, b) => a + b;
+
+const greeting = (name) => `Hello ${name} !`;
+
+const isEven = (number) => (number % 2 == 0 ? true : false);
+
+const Anmimls = ["cat", "dog", "fish"];
+
+const getOrderById = (id) => {
+  if (!id) {
+    throw new Error(" id   is not defined");
+  }
+  return {
+    id: 1,
+    price: 10,
+    date: 2022,
+  };
+};
+
+const getOrders = async () => {
+  return [
+    {
+      id: 1,
+      price: 10,
+    },
+    {
+      id: 2,
+      price: 10,
+    },
+  ];
+};
+
+const applyDiscount = (orderId) => {
+  const order = db.getOrder(orderId);
+  if (order.price >= 10) {
+    order.price -= order.price * 0.1;
+  }
+
+  return order;
+};
+
+module.exports = {
+  sum,
+  greeting,
+  isEven,
+  Anmimls,
+  getOrderById,
+  getOrders,
+  applyDiscount, 
+};
